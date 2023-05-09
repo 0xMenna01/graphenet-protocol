2453c068f8f30e74420fd42e6b643649c38e7561
0xMenna01 <emanuele.cryptdev@gmail.com>
2023-05-09 03:13:33 +0200

    attribute set for cp-abe

---
+++ encryption/src/cpabe/attributes.rs
@@ -0,0 +1,49 @@
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+type Attribute = String;
+
+pub struct AttributeSet {
+    attributes: Vec<Attribute>,
+}
+
+impl AttributeSet {
+    pub fn new() -> Self {
+        let attributes = Vec::new();
+        Self { attributes }
+    }
+
+    pub fn add(&mut self, attribute: Attribute) {
+        if !self.attributes.contains(&attribute) {
+            self.attributes.push(attribute)
+        }
+    }
+
+    pub fn add_all(&mut self, attributes: &mut Vec<Attribute>) {
+        self.attributes.append(attributes);
+    }
+
+    pub fn get(&self, i: usize) -> Option<&Attribute> {
+        self.attributes.get(i)
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &Attribute> {
+        self.attributes.iter()
+    }
+
+    pub fn len(&self) -> usize {
+        self.attributes.len()
+    }
+}
--- encryption/src/cpabe/mod.rs
+++ encryption/src/cpabe/mod.rs
@@ -1,4 +1,3 @@
-// Copyright (C) 2022-2023 Graphenet
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,3 +10,22 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+mod attributes;
+
+use attributes::AttributeSet;
+use ink::primitives::AccountId;
+use rabe::schemes::bsw::{CpAbeMasterKey, CpAbePublicKey, CpAbeSecretKey};
+
+pub enum Error {
+    AttributeCreationFailed,
+    NotAllowed,
+}
+
+pub trait CpAbeScheme {
+    fn setup() -> (CpAbePublicKey, CpAbeMasterKey);
+    fn create_id_for_attributes(account: AccountId, attributes: AttributeSet) -> Result<(), Error>;
+    fn get_key() -> Result<CpAbeSecretKey, Error>;
+}
